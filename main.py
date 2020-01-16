2015ac72671f5c506bcbee51455e71606910468b
Timmy Thatcher <hello@timm.yt>
2020-01-17 00:59:05 +1100

    :art: added some print styling

--- main.py
+++ main.py
@@ -6,9 +6,12 @@ max_number = input("what is the max number you want to study: ")
 cycles = input("how many numbers do you want to study: ")
 sleep_timer = input("how many seconds break: ")
 
+print("-=-=-=-=-=-=-=-=-=-")
+
 for _ in range(int(cycles)):
     rand_num = random.randint(1, int(max_number))
     print(rand_num)
     time.sleep(int(sleep_timer))
     print(num_to_jp(rand_num))
     time.sleep(int(sleep_timer))
+    print("-=-=-=-=-=-=-=-=-=-")
